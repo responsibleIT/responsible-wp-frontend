b3a76b02513c8691a318134ad5c41348d8bc728e
Sanne 't Hooft <shooft@me.com>
2024-12-16 03:26:22 +0100

    no grey-mode, two triangle patterns, slow triangle animation

--- scripts/landmark--triangles.js
+++ scripts/landmark--triangles.js
@@ -1,3 +1,22 @@
+const triangles = document.querySelector(".triangles");
+
+
+///////////////////////////////////////
+// TO TEST IF @PROPERTY IS SUPPORTED //
+///////////////////////////////////////
+
+CSS.registerProperty({
+	name: '--supported',
+	syntax: '*',
+	initialValue: " ",
+	inherits: false
+});
+
+
+
+
+
+
 ///////////////////
 // COLOR LOTTERY //
 /////////////////// 
@@ -7,3 +26,43 @@ const triangleColorRightLottery = Math.floor( Math.random() * 5 ) + 1;
 
 setAttr("data-triangle-color-left-lottery", triangleColorLeftLottery);
 setAttr("data-triangle-color-right-lottery", triangleColorRightLottery);
+
+
+
+
+
+///////////////////
+// PATTERN LOTTERY //
+/////////////////// 
+
+const trianglePatternLeftLottery = Math.floor( Math.random() * 2 ) + 1;
+const trianglePatternRightLottery = Math.floor( Math.random() * 2 ) + 1;
+
+setAttr("data-triangle-pattern-left-lottery", trianglePatternLeftLottery);
+setAttr("data-triangle-pattern-right-lottery", trianglePatternRightLottery);
+
+
+
+
+
+/************************/
+/* RESTART AFTER RESIZE */
+/************************/
+
+let numberTriangles = getComputedStyle(triangles).getPropertyValue("--number-triangles");
+
+
+const resizeObserverTriangles = new ResizeObserver(() => {
+  let newNumberTriangles = getComputedStyle(triangles).getPropertyValue("--number-triangles");
+
+  if (numberTriangles != newNumberTriangles) {
+    numberTriangles = newNumberTriangles;
+
+    triangles.getAnimations().forEach( anim => {
+      anim.cancel();
+			anim.play();
+		});
+  }
+});
+
+resizeObserverTriangles.observe(triangles);
