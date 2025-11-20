9cf8999af29e0c1faeb3eb0c068b142270afb38f
Marcin Magdziak <marcinm@live.nl>
2025-11-20 16:17:35 +0100

    base setup for 11ty

---
+++ .eleventy.js
@@ -0,0 +1,67 @@
+import Image from "@11ty/eleventy-img";
+import { join } from "path";
+import { rm } from "fs/promises";
+import { existsSync } from "fs";
+
+export default function (eleventyConfig) {
+  // Copy everything from public/ to /
+  eleventyConfig.addPassthroughCopy({ "public/": "/" });
+
+  // Remove project-visuals/ after build
+  eleventyConfig.on("eleventy.after", async ({ dir }) => {
+    const projectVisualsPath = join(
+      dir.output,
+      "assets",
+      "img",
+    );
+
+    if (existsSync(projectVisualsPath)) {
+      await rm(projectVisualsPath, { recursive: true, force: true });
+      console.log("Removed:", projectVisualsPath);
+    }
+  });
+
+  // Image generation shortcode
+  eleventyConfig.addLiquidShortcode(
+    "image",
+    async function (src, alt = "", sizes, loading = "lazy") {
+      try {
+        let metadata = await Image(src, {
+          widths: [300, 600, 1200, 1600],
+          formats: ["avif", "webp", "jpeg"],
+          outputDir: "./dist/assets/img/",
+          urlPath: "/assets/img/",
+          transformOnRequest: false, // Disable image transformation
+        });
+
+        let imageAttributes = {
+          alt,
+          sizes: sizes || "(max-width: 768px) 100vw, 50vw",
+          loading,
+          decoding: "async",
+        };
+        // console.log(metadata, "created metadata");
+
+        return Image.generateHTML(metadata, imageAttributes);
+      } catch (error) {
+        // handle missing images (empty image)
+        // console.error(`Error processing image ${src}:`, error);
+        return `<picture><img src="" alt="${alt ? alt : "Image not found"}" /></picture>`;
+      }
+    }
+  );
+
+  return {
+    dir: {
+      input: "views",
+      includes: "partials",
+      layouts: "layouts",
+      data: "../_data", // <-- Go up one level from 'views' to reach root/_data
+      output: "dist",
+    },
+    templateFormats: ["liquid", "md", "html"],
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid",
+    passthroughFileCopy: true,
+  };
+}
