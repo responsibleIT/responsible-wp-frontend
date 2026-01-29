c7fd6014c9fbb4caf35146707a60d9c5e0d07d23
Marcin Magdziak <marcinm@live.nl>
2026-01-29 12:06:23 +0100

    annotation

--- _data/pages.js
+++ _data/pages.js
@@ -1,5 +1,7 @@
 import NormalizedFetch from "../utils/normalizedFetch.js";
 
+// ! _data makes use of utils
+
 export default async function (eleventyData) {
   console.log("Fetching home data...");
   const home = await NormalizedFetch("frontpage", "");
